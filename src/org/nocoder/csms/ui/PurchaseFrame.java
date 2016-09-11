4a70e268188c97c31b15bfa7687180adbcdf9832
yangjinlong <yangjinlong86@gmail.com>
2016-09-11 23:58:30 +0800

    绘制进货管理界面

--- src/org/nocoder/csms/ui/PurchaseFrame.java
+++ src/org/nocoder/csms/ui/PurchaseFrame.java
@@ -1,10 +1,15 @@
 package org.nocoder.csms.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 public class PurchaseFrame extends BaseJFrame {
@@ -17,38 +22,51 @@ public class PurchaseFrame extends BaseJFrame {
 
 	private JPanel createContentPane() {
 		JPanel pane = new JPanel(new BorderLayout());
-
 		pane.add(BorderLayout.NORTH, createInputPane());
-
-		// pane.add(BorderLayout.CENTER, createTablePane());
-
-		pane.add(BorderLayout.SOUTH, new JLabel("蓝氏服装销售管理系统V1.0 - 版权所有，盗版必究", JLabel.RIGHT));
-
+		pane.add(BorderLayout.CENTER, createTablePane());
+		pane.add(BorderLayout.SOUTH, createButtonPanel());
 		return pane;
 	}
 
 	private JPanel createInputPane() {
-		JPanel p = new JPanel(new GridLayout(2, 2, 6, 6));
-		p.add(createBatchPanel());
-		p.add(createBatchPanel());
-		p.add(createBatchPanel());
-		p.add(createBatchPanel());
+		JPanel p = new JPanel(new GridLayout(1, 4));
+		p.add(addInputPanel("批次名称"));
+		p.add(addInputPanel("总成本"));
+		p.add(addInputPanel("运费"));
+		p.add(addInputPanel("备注"));
 		return p;
 	}
 
-	/**
-	 * 批次名称
-	 * 
-	 * @return batchPanel
-	 */
-	private JPanel createBatchPanel() {
-		JPanel p = new JPanel(new BorderLayout(6, 0));
-		p.add(BorderLayout.WEST, new JLabel("批次名称:"));
-		JTextField idField = new JTextField();
-		idField.setText("lanfang");
-		p.add(BorderLayout.CENTER, idField);
-		// 将实例变量idField 引用到界面控件上
-		// this.idField = idField;
-		return p;
+	private JPanel addInputPanel(String label) {
+		JPanel panel = new JPanel(new BorderLayout(6, 0));
+		panel.add(BorderLayout.WEST, new JLabel(label));
+		JTextField textField = new JTextField();
+		panel.add(BorderLayout.CENTER, textField);
+		return panel;
+	}
+
+	private JPanel createTablePane() {
+		final JScrollPane scrollPane = new JScrollPane();
+		JPanel panel = new JPanel(new BorderLayout());
+		JTable table = new JTable(2, 5);
+		table.setGridColor(Color.BLACK);
+		scrollPane.setViewportView(table);
+		panel.add(scrollPane, BorderLayout.CENTER);
+		return panel;
+	}
+
+	private JPanel createButtonPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+		JButton save = new JButton("保存");
+		JButton close = new JButton("关闭");
+		panel.add(save);
+		panel.add(close);
+		getRootPane().setDefaultButton(save);
+		return panel;
+	}
+
+	public static void main(String[] args) {
+		PurchaseFrame purchaseFrame = new PurchaseFrame();
+		purchaseFrame.setVisible(true);
 	}
 }
